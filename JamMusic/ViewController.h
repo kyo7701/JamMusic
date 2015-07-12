aa03837f52d4a131b778c923ed8049e0331e5ea7
24k <24k@24kdeMac.local>
2015-07-12 09:20:25 +0800

    Initial Commit

---
+++ JamMusic/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JamMusic
+//
+//  Created by 24k on 15/7/12.
+//  Copyright (c) 2015年 24k. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ JamMusic/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  JamMusic
+//
+//  Created by 24k on 15/7/12.
+//  Copyright (c) 2015年 24k. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
